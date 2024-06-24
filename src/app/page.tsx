c2d8851ca53cdfe72fc814d3ba764616de73f555
Nikita Lobanov <nikitaalobanovv@gmail.com>
2024-06-23 22:17:36 -0700

    updated styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
    <div >
     <div className="mx-auto items-center text-center">
-   <h1 className="text-7xl text-gray-50 font-semibold my-8 ">Feeling Hungry?</h1>
-   <h3 className="text-2xl text-gray-100 font-semibold my-4">Generate a Random Meal by Clicking Below</h3>
+   <h1 className="text-7xl text-gray-50 font-semibold my-8 mx-4">Feeling Hungry?</h1>
+   <h3 className="text-2xl text-gray-100 font-semibold my-4 mx-4">Generate a Random Meal by Clicking Below</h3>
    </div>
    <RandomMealGenerator/>
    </div>
